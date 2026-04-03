aa674e1e27567594bf961608ea812c19c48a0198
MattiaSilvetri <mattiasilvestri27@gmail.com>
2026-04-03 16:21:06 +0200

    Revert "attempt to use localstorage"
    This reverts commit 4d827f43be98c3a8e315f32774c9e5ccdf9d9386.

--- src/common/utils.js
+++ src/common/utils.js
@@ -1,32 +1,21 @@
 export function openTab(evt, cityName) {
   // Declare all variables 
   let tabcontent, tablinks;
-  const savedTab = localStorage.getItem("tab");
 
-  // Get all elements with class="tabcontent" and hide them if necessary
+  // Get all elements with class="tabcontent" and hide them
   tabcontent = document.getElementsByClassName("tabcontent");
   for (let content of tabcontent) {
-    if (content.id === savedTab) {
-      content.style.display = "block";
-      continue;
-    }
     content.style.display = "none";
   }
 
   // Get all elements with class="tablinks" and remove the class "active"
   tablinks = document.getElementsByClassName("tablinks");
   for (let link of tablinks) {
-    if (link.dataset.city === savedTab) {
-      link.className += " active";
-      continue;
-    }
     link.className = link.className.replace(" active", "");
   }
 
   // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
-  // Save tab in localstorage to open next time
-  localStorage.setItem("tab", cityName);
 }
 
