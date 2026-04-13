b49b133fa9007d211be77e1d6eb874a3953698a2
MattiaSilvestri <mattiasilvestri27@gmail.com>
2026-04-13 15:25:24 +0200

    add iconify

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './style.css'
 import { loadTab, openTab } from "./common/utils";
 import { addTab } from "./common/components";
+import "iconify-icon";
 
 document.addEventListener("DOMContentLoaded", () => {
   // Add link buttons
