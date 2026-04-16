4fb5e1501ac34b0825e67801ec8c4c18a0af8f03
MattiaSilvetri <mattiasilvestri27@gmail.com>
2026-04-16 16:46:46 +0200

    clock span

--- src/components/clock.js
+++ src/components/clock.js
@@ -11,7 +11,7 @@ export class Clock {
     const hh = String(now.getHours()).padStart(2, '0');
     const mm = String(now.getMinutes()).padStart(2, '0');
     const ss = String(now.getSeconds()).padStart(2, '0');
-    if (this.timeEl) this.timeEl.innerHTML = `${hh}:${mm}:${ss}`;
+    if (this.timeEl) this.timeEl.innerHTML = `<span id="clock-h">${hh}</span>:<span id="clock-mm">${mm}</span>:<span id="clock-ss">${ss}</span>`;
     if (this.dateEl) {
       const day = this.days[now.getDay()];
       const month = this.months[now.getMonth()];
