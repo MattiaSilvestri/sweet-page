ec00dfd256cfdeb54c494e693b2663ee0d44ae1a
MattiaSilvestri <mattiasilvestri27@gmail.com>
2026-04-30 14:47:11 +0200

    update banners

--- src/common/defaults.js
+++ src/common/defaults.js
@@ -5,7 +5,14 @@ export const DEFAULT_SETTINGS = {
   "banner": "0_banner.gif",
 }
 
-export const DEFAULT_BANNERS = ["0_banner.gif", "1_banner.gif", "2_banner.jpg", "3_banner.jpg", "4_banner.jpg"]
+export const DEFAULT_BANNERS = [
+  "0_banner.gif", "1_banner.jpg",
+  "10_banner.gif", "11_banner.gif", "12_banner.gif", "13_banner.gif",
+  "14_banner.gif", "15_banner.gif", "16_banner.gif", "17_banner.gif",
+  "18_banner.gif", "19_banner.jpg", "2_banner.jpg", "3_banner.jpg",
+  "4_banner.jpg", "5_banner.webp", "6_banner.jpg", "7_banner.webp",
+  "8_banner.gif", "9_banner.gif"
+]
 
 export const DEFAULT_CONFIG = {
   "bookmarks": {
