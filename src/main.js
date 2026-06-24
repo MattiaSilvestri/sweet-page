cdf7dd980d06acf944bd588e41a92d0dd51fbcda
MattiaSilvestri <mattiasilvestri27@gmail.com>
2026-06-24 16:56:26 +0200

    event listener bookmark

--- src/main.js
+++ src/main.js
@@ -25,8 +25,17 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   // Add Json editor
-  const jsonEditor = new JsonEditor({ target: "jsoneditor-modal" });
+  const jsonEditor = new JsonEditor({ target: "jsoneditor-content" });
   jsonEditor.render();
+
+  const addBookmarkBtn = document.getElementById("add-bookmark");
+  const jsonEditorModal = document.getElementById("jsoneditor-modal");
+  addBookmarkBtn.addEventListener("click", () => {
+    jsonEditorModal.classList.add("open");
+  });
+  jsonEditorModal.addEventListener("click", (e) => {
+    if (e.target === jsonEditorModal) jsonEditorModal.classList.remove("open");
+  });
 
   // Load last tab
   loadTab();
