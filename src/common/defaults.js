2279617e0b45ad1bd36ce92f3c1d4e5c1264ec6d
MattiaSilvestri <mattiasilvestri27@gmail.com>
2026-07-09 15:59:24 +0200

    update banners

--- src/common/defaults.js
+++ src/common/defaults.js
@@ -2,16 +2,14 @@ export const DEFAULT_SETTINGS = {
   "search-engine": "google",
   "open-in-new-tab": false,
   "show-seconds": false,
-  "banner": "0_banner.gif",
+  "banner": "00_banner.gif",
 }
 
 export const DEFAULT_BANNERS = [
-  "0_banner.gif", "1_banner.jpg",
-  "10_banner.gif", "11_banner.gif", "12_banner.gif", "13_banner.gif",
-  "14_banner.gif", "15_banner.gif", "16_banner.gif", "17_banner.gif",
-  "18_banner.gif", "19_banner.jpg", "2_banner.jpg", "3_banner.jpg",
-  "4_banner.jpg", "5_banner.webp", "6_banner.jpg", "7_banner.webp",
-  "8_banner.gif", "9_banner.gif"
+  "00_banner.gif", "01_banner.gif", "02_banner.gif", "03_banner.gif",
+  "04_banner.gif", "05_banner.gif", "06_banner.gif",
+  "07_banner.jpg", "08_banner.jpg", "09_banner.jpg", "10_banner.jpg",
+  "11_banner.webp", "12_banner.jpg", "13_banner.webp", "14_banner.jpg"
 ]
 
 export const DEFAULT_CONFIG = {
